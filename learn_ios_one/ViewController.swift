1ea6a13748c09166efd2bb4612f7e470729737f9
Sarik Kumpan <kumpan.sarik@gmail.com>
2019-08-08 14:04:30 +0700

    Initial Commit

---
+++ learn_ios_one/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  learn_ios_one
+//
+//  Created by Sarik Kumpan on 8/8/2562 BE.
+//  Copyright © 2562 SandBlue. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
